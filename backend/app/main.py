e87f00810e0d5f13741e67b6afaa0b232ede8a22
Wojo <matt.wojtkun@gmail.com>
2020-10-26 13:56:33 -0700

    refactor backend Docerfile, add debugging for backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,17 @@
-from typing import Optional
-
 from fastapi import FastAPI
+import ptvsd
+
+ptvsd.enable_attach(address=('0.0.0.0', 5678))
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World!"}
+    return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    a = 1
+    b = "two"
+    c = 3
+    return {"item_id": item_id}
